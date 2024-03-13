f63784abc9dfe9ef82f9146ca53d27211cbb9576
Petar Shikrenov <shikrenov03@abv.bg>
2024-03-13 20:27:58 +0200

    wip  f

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c8987a4bcdfbaa52"; }
+/******/ 		__webpack_require__.h = function() { return "1549fff6f482e0fd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
